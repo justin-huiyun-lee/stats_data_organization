0e0776496ddf57dd40acb2c31fabd00308d1f400
Justin Lee <justin.huiyun.lee@gmail.com>
2024-05-27 16:55:48 -0700

    ignore

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
+
 fn main() {
-    let data = read_file();
-    match data {
-        Ok(data) => println!("{}", data),
-        Err(e) => println!("Error: {}", e),
-    }
-    println!("{}", data);
 }
 
 
 fn read_file() -> Result<String, io::Error> {
-    let mut file = File::open("data.txt")?;
+    let mut file = File::open("hello.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
